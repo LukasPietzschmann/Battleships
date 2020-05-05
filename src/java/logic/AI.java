d026c3ae8fd91ba00ad95d1a20831a9a6d2b0594
Vincent Ugrai <ugrai.vincent@gmail.com>
2020-05-05 14:13:26 +0200

    add AI class

---
+++ src/java/logic/AI.java
@@ -0,0 +1,18 @@
+package logic;
+
+public class AI {
+
+    private Difficulty difficulty;
+
+    Public void AI(Logic i, int a, String n, Difficulty d){
+
+    }
+
+    public boolean doWhatYouHaveToDo(){
+        return false;
+    }
+
+    public void placeShips(){
+
+    }
+}
