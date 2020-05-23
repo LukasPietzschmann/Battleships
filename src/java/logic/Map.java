b66b8eea9598bd8d1a118d8f03b095236b413c29
Lukas Pietzschmann <lukaspie00@gmail.com>
2020-05-23 15:51:23 +0200

    Fixed Map

--- src/java/logic/Map.java
+++ src/java/logic/Map.java
@@ -1,93 +1,91 @@
 package logic;
 
 public class Map {
-    MapTile [][] map;
-
-    public void dump() {
-        for (MapTile[] mapTiles : map) {
-            System.out.println("|");
-            for (int j = 0; j < mapTiles.length; j++) {
-                String s = "";
-                switch (mapTiles[j].stat) {
-                    case ship:
-                        s = "S";
-                        break;
-                    case water:
-                        s = "~";
-                        break;
-                    case unscuccHit:
-                        s = "/";
-                        break;
-                    case definitleyNoShip:
-                        s = "#";
-                        break;
-                }
-                System.out.print(String.format("%s|", s));
-            }
-            System.out.print("\n");
-        }
-    }
-
-    public int getSize() {
-        return map.length;
-    }
-
-    //public MapStat getStat(int a, int b) {
-
-    //}
-
-    public boolean hit(int x, int y) {
-        if(map[y][x].equals(MapTile.ship)){
-            map[y][x] = MapTile.setStat(succHit);
-            return true;
-        }
-        else{
-            return false;
-        }
-    }
-
-
-    public void Map(int size) {
-        //double root = Math.sqrt(size);
-        //int mapsize = (int)root;
-        map = new MapTile [size][size];
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                map[i][j] = new MapTile();
-            }
-
-        }
-
-        //new MapTile(0, null);
-        //new MapTile(1, new Ship(...));
-
-    }
-    static class MapTile{
-        int stat;
-        public static final int Water = 0;
-        public static final int ship = 1;
-        public static final int succHit = 2;
-        public static final int unsuccHit = 3;
-        // kreuze um ein bereits komplett zerstörtes Schiff, da da keins mehr sein darf
-        public static final int definitelyNoShip = 4;
-        Ship ship;
-
-
-        private MapTile(){
-            stat = Water;
-            ship = null;
-        }
-
-        private void setStat(int stat){
-            this.stat = stat;
-        }
-
-        private void setShip(Ship ship){
-            this.ship = ship;
-        }
-
-        public void setStat(MapStat stat){
-            this.stat = stat;
-        }
-    }
+	private MapTile[][] map;
+	
+	public void Map(int size) {
+		map = new MapTile[size][size];
+		for(int i = 0; i < size; i++) {
+			for(int j = 0; j < size; j++) {
+				map[i][j] = new MapTile();
+			}
+		}
+	}
+	
+	public void dump() {
+		for(MapTile[] mapTiles : map) {
+			System.out.println("|");
+			for(int j = 0; j < mapTiles.length; j++) {
+				String s = "";
+				switch(mapTiles[j].stat) {
+					case MapTile.SHIP:
+						s = "S";
+						break;
+					case MapTile.WATER:
+						s = "~";
+						break;
+					case MapTile.UNSUCCHIT:
+						s = "/";
+						break;
+					case MapTile.DEFINITELYNOSHIP:
+						s = "#";
+						break;
+				}
+				System.out.print(String.format("%s|", s));
+			}
+			System.out.print("\n");
+		}
+	}
+	
+	public int getSize() {
+		return map.length;
+	}
+	
+	//public MapStat getStat(int a, int b) {
+	
+	//}
+	
+	public boolean hit(int x, int y) {
+		if(map[y][x].stat == MapTile.WATER) {
+			map[y][x].stat = MapTile.UNSUCCHIT;
+			return false;
+		}
+		if(map[y][x].stat == MapTile.UNSUCCHIT) return false;
+		if(map[y][x].stat == MapTile.SUCCHIT) return false;
+		if(map[y][x].stat == MapTile.DEFINITELYNOSHIP) return false;
+		if(map[y][x].stat == MapTile.SHIP) {
+			map[y][x].stat = MapTile.SUCCHIT;
+			map[y][x].ship.hit();
+			
+			if(!map[y][x].ship.isAlive()) {
+				// TODO hier muss dann noch überprüft werden ob das Schiff versenkt wurde
+				// Falls ja muss die Variable die speichert wie viele Schiffe noch
+				// "leben" verringert werden
+			}
+			
+			return true;
+		}
+		
+		return false;
+	}
+	
+	static class MapTile {
+		public static final int WATER = 0;
+		public static final int SHIP = 1;
+		public static final int SUCCHIT = 2;
+		public static final int UNSUCCHIT = 3;
+		// kreuze um ein bereits komplett zerstörtes Schiff, da da keins mehr sein darf
+		public static final int DEFINITELYNOSHIP = 4;
+		private Ship ship;
+		private int stat;
+		
+		private MapTile() {
+			stat = WATER;
+			ship = null;
+		}
+		
+		// Die setter hab ich doch raus gemacht weil mir eingefallen ist,
+		// dass man auf die Attribute auch so zugreifen kann (da es eine
+		// innere Klasse ist)
+	}
 }
