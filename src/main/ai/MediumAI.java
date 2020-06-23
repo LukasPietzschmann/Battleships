6cddc36f212064dc7bd552f95ce7199f32ccda9d
Lukas Pietzschmann <lukaspie00@gmail.com>
2020-06-23 17:41:22 +0200

    Improved Medium AI

--- src/main/ai/MediumAI.java
+++ src/main/ai/MediumAI.java
@@ -7,6 +7,9 @@ import logic.Player;
 import logic.Ship;
 import logic.Ship.Direction;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Random;
 
 public class MediumAI extends PlayableAI {
@@ -20,7 +23,7 @@ public class MediumAI extends PlayableAI {
 	 *
 	 * @return {@inheritDoc}
 	 */
-	@Override
+	/*@Override
 	public boolean makeMove() {
 		Random rnd = new Random();
 		int x, y;
@@ -32,15 +35,15 @@ public class MediumAI extends PlayableAI {
 			y = rnd.nextInt(map.getSize());
 			
 			if(logic.shoot(x, y, player) == null) return false; // Wenn nicht getroffen wurde
-			else {
-				// Wenn getroffen wurde, wird der Punkt gespeichert
-				lastXPos = x;
-				lastYPos = y;
-				return true;
-			}
+			// Wenn getroffen wurde, wird der Punkt gespeichert
+			lastXPos = x;
+			lastYPos = y;
+			return true;
 		}else if(lastDir != null && lastXPos != -1 && lastYPos != -1) {
 			x = lastXPos;
 			y = lastYPos;
+			//Hier gehts immer hin und her wenn ich am ende bin, es aber nicht versenkt ist.
+			//Ich möchte dann wieder am Anfang anfangen und in die entgegengesetzte Richtung gehen
 			if(isValidDirection(lastDir, lastXPos, lastYPos)) dir = lastDir;
 			else dir = mirrorDirection(lastDir);
 			// Falls ich da weiter machen kann
@@ -62,19 +65,46 @@ public class MediumAI extends PlayableAI {
 			}
 			return true;
 		}else if(lastDir == null && lastXPos != -1 && lastYPos != -1) {
-			while(!isValidDirection(dir = Direction.north, lastXPos, lastYPos)) {
+			do {
 				dir = Direction.values()[rnd.nextInt(Direction.values().length)];
-			}
+			}while(!isValidDirection(dir, lastXPos, lastYPos));
 			if(shootInDirection(dir, lastXPos, lastYPos) == null) return false;
-			else {
-				lastXPos = getNewXKoord(dir, lastXPos);
-				lastYPos = getNewYKoord(dir, lastYPos);
-				lastDir = dir;
-				return true;
-			}
+			lastXPos = getNewXKoord(dir, lastXPos);
+			lastYPos = getNewYKoord(dir, lastYPos);
+			lastDir = dir;
+			return true;
 		}
 		
 		System.err.println("Fuck die AI geht doch net einwandfrei");
 		return false;
+	}*/
+	@Override
+	protected boolean makeMove() {
+		int x, y;
+		if(currMission == null) {
+			Random rnd = new Random();
+			do {
+				x = rnd.nextInt(map.getSize());
+				y = rnd.nextInt(map.getSize());
+			}while(enemyMap[y][x] == NO_SHIP);
+			
+			if(logic.shoot(x, y, player) == null) return false;
+			currMission = new Mission(x, y, map);
+			return true;
+		}
+		
+		x = currMission.getNextX();
+		y = currMission.getNextY();
+		
+		Ship ship = logic.shoot(x, y, player);
+		if(ship == null) {
+			currMission.wasHit(false);
+			return false;
+		}
+		enemyMap[y][x] = NO_SHIP;
+		if(ship.isAlive()) currMission.wasHit(true);
+		else currMission = null;
+		
+		return true;
 	}
 }
